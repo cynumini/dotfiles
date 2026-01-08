b1f4bc7b7788e220e91faa3a7e4408b0907216a8
cynumini <cynumini@proton.me>
2026-01-08 17:07:00 +0200

    2026-01-08

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out,                            "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,97 +21,114 @@ vim.g.maplocalleader = "\\"
 -- Theme & transparency
 vim.cmd.colorscheme("retrobox")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
 
 -- Basic settings
 vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- Indentation
-vim.o.tabstop = 2    -- how many spaces in tab
-vim.o.shiftwidth = 2 -- how many spaces for indentation
-vim.o.smartindent = true
+vim.o.tabstop = 4 -- how many spaces in tab
+-- vim.o.shiftwidth = 4 -- how many spaces for indentation
+-- vim.o.smartindent = true
 vim.o.list = true
-vim.o.expandtab = true
+-- vim.o.expandtab = true
 
 -- Search settings
-vim.o.ignorecase = true
-vim.o.smartcase = true
+-- vim.o.ignorecase = true
+-- vim.o.smartcase = true
 
 -- File handling
-vim.o.writebackup = false
-vim.o.swapfile = false
-vim.o.undofile = true
+-- vim.o.writebackup = false
+-- vim.o.swapfile = false
+-- vim.o.undofile = true
 
 -- End of half
-vim.o.colorcolumn = "116"
+-- vim.o.colorcolumn = "116"
 
 -- Behavior settings
 vim.o.clipboard = "unnamedplus"
 
--- Basic autocommands
-local augroup = vim.api.nvim_create_augroup("UserConfig", {})
-
--- Create directories when saving files
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = augroup,
-  callback = function()
-    -- Skip for Oil buffers or unnamed buffers
-    if vim.bo.filetype == "oil" or vim.api.nvim_buf_get_name(0) == "" then
-      return
-    end
-
-    local dir = vim.fs.dirname(vim.api.nvim_buf_get_name(0))
-    if vim.fn.isdirectory(dir) == 0 then
-      vim.fn.mkdir(dir, "p")
-    end
-  end,
+-- Setup lazy.nvim
+require("lazy").setup({
+	spec = {
+		'neovim/nvim-lspconfig',
+		{
+			'nvim-telescope/telescope.nvim',
+			tag = 'v0.2.1',
+			dependencies = {
+				'nvim-lua/plenary.nvim',
+				-- optional but recommended
+				{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+			}
+		},
+		{
+			'saghen/blink.cmp',
+			dependencies = { 'rafamadriz/friendly-snippets' },
+			version = '1.*',
+			---@module 'blink.cmp'
+			---@type blink.cmp.Config
+			opts = {
+				keymap = { preset = 'default' },
+				appearance = { nerd_font_variant = 'normal' },
+				completion = { documentation = { auto_show = false } },
+				sources = {
+					default = { 'lsp', 'path', 'snippets', 'buffer' },
+					providers = { lsp = { fallbacks = {} } }
+				},
+				fuzzy = { implementation = "prefer_rust_with_warning" }
+			},
+			opts_extend = { "sources.default" }
+		}
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "retrobox" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
 })
 
-require("lazy").setup({
-  spec = {
-    'https://github.com/neovim/nvim-lspconfig',
-    {
-      'stevearc/oil.nvim',
-      ---@module 'oil'
-      ---@type oil.SetupOpts
-      opts = { view_options = { show_hidden = true } },
-      dependencies = { { "echasnovski/mini.icons", opts = {} } },
-      lazy = false,
-    },
-    {
-      'nvim-telescope/telescope.nvim',
-      tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {
-      'saghen/blink.cmp',
-      dependencies = { 'rafamadriz/friendly-snippets' },
-      version = '1.*',
-      ---@module 'blink.cmp'
-      ---@type blink.cmp.Config
-      opts = {
-        keymap = { preset = 'default' },
-        appearance = { nerd_font_variant = 'mono' },
-        completion = { documentation = { auto_show = false } },
-        sources = {
-          default = { 'lsp', 'path', 'snippets', 'buffer' },
-          providers = { lsp = { fallbacks = {} } }
-        },
-        fuzzy = { implementation = "prefer_rust_with_warning" }
-      },
-      opts_extend = { "sources.default" }
-    }
-  },
-  install = { colorscheme = { "retrobox" } },
-  checker = { enabled = true },
+-- Support for config
+vim.lsp.config('lua_ls', {
+	on_init = function(client)
+		if client.workspace_folders then
+			local path = client.workspace_folders[1].name
+			if path ~= vim.fn.stdpath('config') and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then
+				return
+			end
+		end
+		client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+			runtime = {
+				version = 'LuaJIT',
+				path = {
+					'lua/?.lua',
+					'lua/?/init.lua',
+				},
+			},
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					vim.env.VIMRUNTIME
+				}
+			}
+		})
+	end,
+	settings = {
+		Lua = {}
+	}
 })
 
 require('lua_ls')
 vim.lsp.enable('basedpyright')
 vim.lsp.enable("lua_ls")
-vim.lsp.enable('ltex_plus')
+vim.lsp.enable('ltex_plus', {
+	settings = {
+		ltex = {
+			language = "ja-JP",
+		}
+	}
+})
 vim.lsp.enable('zls')
 vim.lsp.enable('clangd')
 vim.lsp.enable('qmlls')
@@ -121,17 +138,21 @@ vim.lsp.enable('asm_lsp')
 vim.keymap.set('n', '<Leader>nd', require("tasks"))
 
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = augroup,
-  callback = function(args)
-    vim.keymap.set('n', '<Leader>lf', function() vim.lsp.buf.format() end)
-  end
+	callback = function(args)
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+		if client:supports_method('textDocument/formatting') then
+			vim.keymap.set('n', '<leader>lf',
+				function() vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 }) end)
+		end
+	end
 })
 
-vim.keymap.set("n", "<leader>o", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "<leader>o", "<CMD>Ex<CR>", { desc = "Open parent directory" })
 
-local builtin = require('telescope.builtin');
-
+local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>fq', builtin.quickfix, { desc = 'Telescope lists items in the quickfix list' })
