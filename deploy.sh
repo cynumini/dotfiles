a60e684bb773dbb002511135331a643126b4ad18
cynumini <cynumini@proton.me>
2026-05-29 01:13:12 +0300

    add i3 config

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+mkdir $HOME/.config/
+
+stow i3 -t $HOME
--- lib/vars.scm
+++
@@ -1,8 +0,0 @@
-(define-module (vars) #:export (font font-size wallpaper-path terminal))
-
-(use-modules (patchouli))
-
-(define font "Iosevka Nerd Font")
-(define font-size 13)
-(define wallpaper-path (with-home "pictures/wallpaper.png"))
-(define terminal "st -e")
